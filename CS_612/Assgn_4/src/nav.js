1a1b0a29f12825067c7154c54ff7dc691580fdda
abbasshamshi <abbasshamshi72@gmail.com>
2019-11-21 14:43:44 -0500

    News Api Integration

--- CS_612/Assgn_4/src/nav.js
+++ CS_612/Assgn_4/src/nav.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 
 class Nav extends Component {
-    
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null,
+        };
+    }
+    componentDidMount() {
+        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=a7208873a9dd4d9aa4372066f2d6da4a&pageSize=10')
+            .then(response => response.json())
+            .then(data => this.setState({ data }));
+    }
 
+
+
+
+
+    render() {
+        console.log("fare", this.state.data);
         return (
 
             <div className="flex-right">
@@ -11,7 +26,26 @@ class Nav extends Component {
                     <li><a className="nav-link nav-block nav-color nav-align" href="main.js"> Google</a></li>
 
                 </ul>
-                
+                <div className="News"><h3>News</h3>
+
+                    <div>
+                        {
+                            this.state.data != null ?
+                                this.state.data.articles.map(d => {
+                                    return (
+                                        <div className="news_t">
+                                            <h4>Title:</h4> <h5>{d.title}</h5>
+                                        </div>
+                                    )
+                                })
+                                : null
+                        }
+                    </div>
+                </div>
+
+
+
+
             </div>
         );
     }
